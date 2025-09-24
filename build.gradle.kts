02316d038fdda9b78bcce8a695480d90416c7be5
Denis Koljada <10576274+justdeko@users.noreply.github.com>
2025-09-24 22:44:59 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ kotlin {
   sourceSets {
     val commonMain by getting {
       dependencies {
-        implementation("com.github.justdeko:ktmpwasm:-SNAPSHOT") { isChanging = true }
+        implementation("com.github.justdeko:ktmpwasm:-e273fedbc8-1") { isChanging = true }
       }
     }
     val commonTest by getting { dependencies { implementation(kotlin("test")) } }
